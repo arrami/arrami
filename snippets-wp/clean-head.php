e4da9a11fa8fbe96616710b70c94860c41fe43fc
Stéphane Arrami <contact@arrami.me>
2026-09-13 17:47:46 +0200

    docs(readme): version FR épurée et ajout structure snippets

---
+++ snippets-wp/clean-head.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: SAR Clean Head
+ * Description: Supprime les pollutions inutiles du <head> WordPress.
+ * Author: Stéphane Arrami
+ */
+
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+add_action('init', function () {
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'rest_output_link_wp_head');
+    remove_action('wp_head', 'wp_oembed_add_discovery_links');
+
+    // Désactive les emojis WP si non utilisés
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+});
